f7c3d132c7a6daa7cacf9f72d2eb4bd71baf8a17
Rafael Fernandes <rafael.ferper@gmail.com>
2024-03-25 16:42:10 -0300

    Ajustando criação de restaurante

--- src/test/java/com/github/rafaelfernandes/restaurant/adapter/in/web/RestaurantControllerTest.java
+++ src/test/java/com/github/rafaelfernandes/restaurant/adapter/in/web/RestaurantControllerTest.java
@@ -1,211 +1,224 @@
 package com.github.rafaelfernandes.restaurant.adapter.in.web;
 
+import com.github.rafaelfernandes.restaurant.adapter.in.web.request.AddressRequest;
+import com.github.rafaelfernandes.restaurant.adapter.in.web.request.CuisineRequest;
+import com.github.rafaelfernandes.restaurant.adapter.in.web.request.OpeningHourRequest;
 import com.github.rafaelfernandes.restaurant.adapter.in.web.request.RestaurantRequest;
-import com.github.rafaelfernandes.restaurant.application.domain.model.Restaurant;
-import com.github.rafaelfernandes.restaurant.application.port.in.GetRestaurantUseCase;
-import com.github.rafaelfernandes.restaurant.application.port.in.SaveDataRestaurantUseCase;
-import com.github.rafaelfernandes.common.exception.RestaurantDuplicateException;
-import com.github.rafaelfernandes.common.exception.RestaurantNotFoundException;
+import com.github.rafaelfernandes.restaurant.adapter.out.persistence.RestaurantRepository;
+import com.github.rafaelfernandes.common.enums.State;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.http.MediaType;
 import util.GenerateData;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-@WebMvcTest(controllers = RestaurantController.class)
-class RestaurantControllerTest {
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RestaurantControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private TestRestTemplate restTemplate;
 
-    @MockBean
-    private SaveDataRestaurantUseCase saveDataRestaurantUseCase;
+    @Autowired
+    private RestaurantRepository restaurantRepository;
 
-    @MockBean
-    private GetRestaurantUseCase getRestaurantUseCase;
+    @BeforeEach
+    void setup(){
+        restTemplate.getRestTemplate().setInterceptors(
+                Collections.singletonList((request, body, execution) -> {
+                    request.getHeaders()
+                            .add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+                    return execution.execute(request, body);
+                }));
+    }
+
+    @AfterEach
+    void tearDown(){
+        restaurantRepository.deleteAll();
+    }
+
+    List<OpeningHourRequest> openingHours = GenerateData.generateOpeningHoursRequest();
+
+    List<CuisineRequest> cuisines = GenerateData.generateCuisinesRequest();
 
     @Nested
     class Create {
+
         @Test
-        void validateCreateRestaurantCommandError() throws Exception {
+        void createSuccess() {
 
-            var address = GenerateData.generateAddressRequest();
+            RestaurantRequest request = GenerateData.gerenRestaurantRequest();
 
-            String name = null;
+            ResponseEntity<String> create = createRestaurantPost(request);
 
-            var openingHours = GenerateData.generateOpeningHoursRequest();
+            assertThat(create.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
-            var cuisines = GenerateData.generateCuisinesRequest();
+            URI location = create.getHeaders().getLocation();
 
-            RestaurantRequest restaurant = new RestaurantRequest(name, address, 10, openingHours, cuisines);
+            ResponseEntity<String> response = restTemplate
+                    .getForEntity(
+                            location,
+                            String.class
+                    );
 
-            mockMvc.perform(
-                            post("/restaurants/")
-                                    .contentType(MediaType.APPLICATION_JSON)
-                                    .content(GenerateData.asJsonString(restaurant))
-                    )
-                    .andExpect(status().isBadRequest())
-                    .andExpect(jsonPath("$.errors").value("name: O campo deve estar preenchido"))
-            ;
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
 
         }
 
         @Test
-        void validateDuplicateRestaurantError() throws Exception {
+        void duplicateNameError() {
 
-            var restaurant = GenerateData.gerenRestaurantRequest();
+            AddressRequest addressRequest = GenerateData.generateAddressRequest();
+            String name = "COMIDA BOA";
 
-            when(saveDataRestaurantUseCase.create(any(Restaurant.class)))
-                    .thenThrow(RestaurantDuplicateException.class);
+            RestaurantRequest request = new RestaurantRequest(name, addressRequest, 10, openingHours, cuisines);
 
-            mockMvc.perform(
-                            post("/restaurants/")
-                                    .contentType(MediaType.APPLICATION_JSON)
-                                    .content(GenerateData.asJsonString(restaurant))
-                    )
-                    .andExpect(status().isConflict())
-                    .andExpect(jsonPath("$.errors").value("Nome já cadastrado!"))
-            ;
+            ResponseEntity<String> response = createRestaurantPost(request);
+
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+            ResponseEntity<String> responseDuplicate = createRestaurantPost(request);
+
+            assertThat(responseDuplicate.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
+
+            DocumentContext documentContext = JsonPath.parse(responseDuplicate.getBody());
+
+            String error = documentContext.read("$.errors");
+
+            assertThat(error).contains("Nome já cadastrado!");
 
         }
 
         @Test
-        void validateCreateSucess() throws Exception {
+        void createInvalidRequest() {
 
-            var restaurant = GenerateData.gerenRestaurantRequest();
+            AddressRequest addressRequest = GenerateData.generateAddressRequest();
+            String name = "";
 
-            var restaurantId = new Restaurant.RestaurantId(UUID.randomUUID().toString());
+            RestaurantRequest request = new RestaurantRequest(name, addressRequest, 10, openingHours, cuisines);
 
-            when(saveDataRestaurantUseCase.create(any(Restaurant.class)))
-                    .thenReturn(restaurantId);
+            ResponseEntity<String> response = createRestaurantPost(request);
 
-            mockMvc.perform(
-                            post("/restaurants/")
-                                    .contentType(MediaType.APPLICATION_JSON)
-                                    .content(GenerateData.asJsonString(restaurant))
-                    )
-                    .andExpect(status().isCreated())
-                    .andExpect(header().string("Location", containsString("/restaurants/"+restaurantId.id())));
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
 
-            ;
+            DocumentContext documentContext = JsonPath.parse(response.getBody());
 
-        }
-    }
+            String error = documentContext.read("$.errors");
 
-    @Nested
-    class FindById{
-
-        @Test
-        void validateCommandError() throws Exception {
-            mockMvc.perform(
-                            get("/restaurants/uuid-invalid")
-                                    .contentType(MediaType.APPLICATION_JSON)
-                    )
-                    .andExpect(status().isBadRequest())
-                    .andExpect(jsonPath("$.errors").value("id: O campo deve ser do tipo UUID"))
-            ;
-        }
-
-        @Test
-        void validateNotFound() throws Exception {
-
-            when(getRestaurantUseCase.findById(any(Restaurant.RestaurantId.class)))
-                    .thenThrow(RestaurantNotFoundException.class);
-
-            mockMvc.perform(
-                            get("/restaurants/e903732e-9d20-4023-a71a-5c761253fc1c")
-                                    .contentType(MediaType.APPLICATION_JSON)
-                    )
-                    .andExpect(status().isNotFound())
-                    .andExpect(jsonPath("$.errors").value("Restaurante(s) não existe!"))
-            ;
-        }
-
-        @Test
-        void validateFound() throws Exception {
-
-            var restaurant = Optional.of(GenerateData.createRestaurant());
-
-            when(getRestaurantUseCase.findById(any(Restaurant.RestaurantId.class)))
-                    .thenReturn(restaurant);
-
-            mockMvc.perform(
-                            get("/restaurants/e903732e-9d20-4023-a71a-5c761253fc1c")
-                                    .contentType(MediaType.APPLICATION_JSON)
-                    )
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.id").value(restaurant.get().getRestaurantId().id().toString()))
-                    .andExpect(jsonPath("$.name").value(restaurant.get().getName()))
-                    .andExpect(jsonPath("$.address.street").value(restaurant.get().getAddress().getStreet()))
-                    .andExpect(jsonPath("$.address.number").value(restaurant.get().getAddress().getNumber()))
-                    .andExpect(jsonPath("$.address.addittionalDetails").value(restaurant.get().getAddress().getAddittionalDetails()))
-                    .andExpect(jsonPath("$.address.neighborhood").value(restaurant.get().getAddress().getNeighborhood()))
-                    .andExpect(jsonPath("$.address.city").value(restaurant.get().getAddress().getCity()))
-                    .andExpect(jsonPath("$.address.state").value(restaurant.get().getAddress().getState()))
-            ;
-
-        }
-
-
-    }
-
-    @Nested
-    class FindAllBy {
-
-        void validateNotFound() throws Exception {
-
-            when(getRestaurantUseCase.findAllBy(any(String.class), any(String.class), anyList()))
-                    .thenThrow(RestaurantNotFoundException.class);
-
-            mockMvc.perform(
-                            get("/restaurants/")
-                                    .contentType(MediaType.APPLICATION_JSON)
-                    )
-                    .andExpect(status().isNotFound())
-                    .andExpect(jsonPath("$.errors").value("Restaurante(s) não existe!"))
-            ;
-        }
-
-        @Test
-        void validateFound() throws Exception {
-
-            var restaurant = Optional.of(GenerateData.createRestaurant());
-
-            when(getRestaurantUseCase.findById(any(Restaurant.RestaurantId.class)))
-                    .thenReturn(restaurant);
-
-            mockMvc.perform(
-                            get("/restaurants/e903732e-9d20-4023-a71a-5c761253fc1c")
-                                    .contentType(MediaType.APPLICATION_JSON)
-                    )
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.id").value(restaurant.get().getRestaurantId().id().toString()))
-                    .andExpect(jsonPath("$.name").value(restaurant.get().getName()))
-                    .andExpect(jsonPath("$.address.street").value(restaurant.get().getAddress().getStreet()))
-                    .andExpect(jsonPath("$.address.number").value(restaurant.get().getAddress().getNumber()))
-                    .andExpect(jsonPath("$.address.addittionalDetails").value(restaurant.get().getAddress().getAddittionalDetails()))
-                    .andExpect(jsonPath("$.address.neighborhood").value(restaurant.get().getAddress().getNeighborhood()))
-                    .andExpect(jsonPath("$.address.city").value(restaurant.get().getAddress().getCity()))
-                    .andExpect(jsonPath("$.address.state").value(restaurant.get().getAddress().getState()))
-            ;
+            assertThat(error).contains("name: O campo deve estar preenchido");
 
         }
 
     }
 
+    @Nested
+    class FindById {
 
+        @Test
+        void validateCommandError() {
+
+            ResponseEntity<String> response = restTemplate
+                    .getForEntity(
+                    "/restaurants/uuid-invalid",
+                        String.class
+                    );
+
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+
+            DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+            String error = documentContext.read("$.errors");
+
+            assertThat(error).isEqualTo("id: O campo deve ser do tipo UUID");
+
+        }
+
+        @Test
+        void validateNotFound() {
+            ResponseEntity<String> response = restTemplate
+                    .getForEntity(
+                            "/restaurants/e903732e-9d20-4023-a71a-5c761253fc1c",
+                            String.class
+                    );
+
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+
+            DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+            String error = documentContext.read("$.errors");
+
+            assertThat(error).isEqualTo("Restaurante(s) não existe!");
+        }
+
+
+        @Test
+        void validateFound(){
+
+            var request = GenerateData.gerenRestaurantRequest();
+
+            ResponseEntity<String> create = createRestaurantPost(request);
+
+            URI location = create.getHeaders().getLocation();
+
+            ResponseEntity<String> response = restTemplate
+                    .getForEntity(
+                            location,
+                            String.class
+                    );
+
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+            DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+            String name = documentContext.read("$.name");
+            assertThat(name).isEqualTo(request.name());
+
+            String addressStreet = documentContext.read("$.address.street");
+            assertThat(addressStreet).isEqualTo(request.address().street());
+
+            Integer addressNumber = documentContext.read("$.address.number");
+            assertThat(addressNumber).isEqualTo(request.address().number());
+
+            String addressAddittionalDetails = documentContext.read("$.address.addittionalDetails");
+            assertThat(addressAddittionalDetails).isEqualTo(request.address().addittionalDetails());
+
+            String addressNeighborhood = documentContext.read("$.address.neighborhood");
+            assertThat(addressNeighborhood).isEqualTo(request.address().neighborhood());
+
+            String addressCity = documentContext.read("$.address.city");
+            assertThat(addressCity).isEqualTo(request.address().city());
+
+            String addressState = documentContext.read("$.address.state");
+            assertThat(addressState).isEqualTo(request.address().state());
+
+        }
+
+    }
+
+    private ResponseEntity<String> createRestaurantPost(RestaurantRequest request) {
+        return restTemplate
+                .postForEntity(
+                        "/restaurants/",
+                        request,
+                        String.class
+                );
+
+    }
 
 }
